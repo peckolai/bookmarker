bd78189860bc554e047d3be2dcaa0277fabb4435
Nikolay <nikolay.i.nikolov@mentormate.com>
2021-08-12 15:37:17 +0300

    3

---
+++ app/controllers/concerns/renderer.rb
@@ -0,0 +1,12 @@
+module Renderer
+  def render_object(resource, **options )
+    options.merge!(json: resource, root: :data)
+    options.merge!(status: :ok) unless options.key?(:status)
+
+    render options
+  end
+
+  def render_errors(errors, status = :unprocessable_entity)
+    render json: errors, status: status
+  end
+end
